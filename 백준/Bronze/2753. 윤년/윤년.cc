458bb2934cac1c99b0333b062a61116cbc050393
khwook1008 <35060231+khwook1008@users.noreply.github.com>
2024-01-08 10:43:28 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int year;
+    scanf("%d", &year);
+    if(year % 4 == 0){
+        if(year % 100 != 0 || year % 400 == 0){
+            printf("1");
+        }
+        else{
+            printf("0");
+        }
+    }
+    else{
+        printf("0");
+    }
+	return 0;
+}
