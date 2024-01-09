00cafaf5c5a9580ed1e220405e8734aea42165fc
khwook1008 <35060231+khwook1008@users.noreply.github.com>
2024-01-09 10:35:27 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,16 +3,19 @@
 using namespace std;
 
 string str;
-char c;
+char a; // 주어진 문자
+long long M = 1234567891; // 서로소 M
 int main(){
     int L;
     long long hash = 0;
     scanf("%d",&L);
     cin >> str;
 
-    for(int i=0; i<str.size(); i++){
-        c = str[i];
-        hash += (c - 96) * pow(31, i);
+    long long r = 1;
+    for(int i=0; i<L; i++){
+        a = str[i];
+        hash = (hash + (a - 96) * r) % M; // (a * r) mod M
+        r = (r * 31) % M; // pow(r, n); -> 값이 너무 커지니까 계속 mod M 해준다.
     }
 
     printf("%lld\n", hash);
