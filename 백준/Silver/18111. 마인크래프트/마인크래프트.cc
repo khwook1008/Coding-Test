872cc9a9814571a15105ef7f63a4abdf61bd3887
khwook1008 <35060231+khwook1008@users.noreply.github.com>
2024-02-05 12:11:26 +0900

    마인크래프트.cc

--- 백준/Silver/18111. 마인크래프트/마인크래프트.cc
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -1,41 +1,50 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+
 using namespace std;
+
 int n,m,b;
 int a[504][504];
-int mn = 987654321;
-int h;
-int main(void){
-	ios_base::sync_with_stdio(0);
+int mintime = 987654321;
+int height;
+	
+int main(){
+    
+    ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
+
 	cin >> n >> m >> b;
-	for(int i=0; i<n; i++){
-		for(int j=0; j<m; j++){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+		    //입력 받기
 			cin >> a[i][j];
 		}
 	}
 	
-	for(int e = 0; e <= 256; e++){
-		int x = 0; //build
-		int y = 0; //remove
-		for(int i=0; i<n; i++){
-			for(int j=0; j<m; j++){
-				if(a[i][j] > e) {
-					y += a[i][j] - e;
-				} else if(a[i][j] < e){
-					x += e - a[i][j];
+	for(int h = 0; h <= 256; h++){ //brute force 0 <= h <= 256
+		int add = 0;
+		int remove = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+			    //맞출 height보다 높으면 제거
+				if(a[i][j] > h) {
+					remove += a[i][j] - h;
+				}
+				//맞출 height보다 낮으면 쌓기
+				else if(a[i][j] < h){
+					add += h - a[i][j];
 				}
 			}
 		}
-		if(y + b >= x) {
-			int time = y * 2 + x;
-			if(mn >= time){
-				mn = time;
-				h = e;
+		if(remove + b >= add) {
+			int time = remove * 2 + add;
+			if(mintime >= time){
+				mintime = time;
+				height = h;
 			}
 		}
 	}
-	cout << mn << " " << h;
+	cout << mintime << " " << height;
 	return 0;
 }
