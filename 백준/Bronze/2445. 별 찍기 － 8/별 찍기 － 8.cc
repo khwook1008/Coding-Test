f0c03c129a497ae070817a55b7d96848d8f1563e
khwook1008 <35060231+khwook1008@users.noreply.github.com>
2024-02-27 00:05:09 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        for(int j = 1; j <= 2* N - i * 2; j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }        
+        cout << '\n';
+    }
+    
+    for(int i = N; i > 0; i--){
+        for(int j = 1; j <= i - 1; j++){
+            cout << "*";
+        }
+        for(int j = 1; j <= 2 * (N - i + 1); j++){
+            cout << " ";
+        }
+        for(int j = 1; j <= i - 1; j++){
+            cout << "*";
+        }        
+        cout << '\n';
+    }
+
+    return 0;
+}
