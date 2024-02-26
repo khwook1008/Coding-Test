ca3507868cb347c25d29d31dcaa2e7d6535c5fc0
khwook1008 <35060231+khwook1008@users.noreply.github.com>
2024-02-26 22:35:13 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N;
+
+    cin >> N;
+    
+    /*for(int i = 0; i < N; i++){
+        for(int j = 0; j < N - i; j++){
+            cout << '*';
+        }
+        cout << '\n';
+    }*/
+    
+    for(int i = 0; i < N; i++){
+        for(int j = N; j - i > 0; j--){
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
