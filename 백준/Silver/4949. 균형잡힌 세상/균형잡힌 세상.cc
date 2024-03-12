af401dbb5b702bec91b4c037df2547becfac418a
khwook1008 <35060231+khwook1008@users.noreply.github.com>
2024-03-13 00:00:00 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,54 +1,56 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <stack>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    while (1) {
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while(1){
+        
         string str;
-        getline(cin, str);
- 
-        if (str == ".") {
+        
+        getline(cin, str); //한줄을 한꺼번에 다 입력 받아야한다.
+        
+        if(str == "."){ //'.' string이 한글자 들어오면 종료
             break;
         }
- 
-        stack<char> s;
-        bool flag = 0;
-        for (int i = 0; i < str.length(); i++) {
+        
+        stack<char> st;
+        int no = 0;
+        
+        for(int i = 0; i < str.length(); i++){
             char c = str[i];
-                
-            if ((c == '(') || (c == '[')) {
-                s.push(c);
+            if(c == '(' || c == '['){
+                st.push(c);
             }
-            else if (c == ')') {
-                if (!s.empty() && s.top() == '(') {
-                    s.pop();
-                }
-                else {
-                    flag = 1;
+            else if(c == ')'){
+                if(st.empty() || st.top() != '('){
+                    no = 1;
                     break;
                 }
-            }
-            else if (c == ']') {
-                if (!s.empty() && s.top() == '[') {
-                    s.pop();
+                else{
+                    st.pop();
                 }
-                else {
-                    flag = 1;
+            }
+            else if(c == ']'){
+                if(st.empty() || st.top() != '['){
+                    no = 1;
                     break;
                 }
+                else{
+                    st.pop();
+                }
             }
         }
- 
-        if (flag==0 && s.empty()) {
-            cout << "yes" << endl;
+        if(!st.empty()){
+            no = 1;
         }
-        else {
-            cout << "no" << endl;
+        if(no == 0){
+            cout << "yes\n";
         }
- 
+        else{
+            cout << "no\n";
+        }
     }
+    return 0;
 }
