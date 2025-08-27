29fed82357238d841aa24c21f4e673f8c5fbf81e
khwook1008 <35060231+khwook1008@users.noreply.github.com>
2025-08-27 17:08:41 +0900

    [Silver II] Title: 에디터, Time: 60 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,51 +1,53 @@
-#include <iostream>
-#include <list>
+// 1406 실버2 에디터
 
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
-  list<char> a;
-  string str;
-  cin >> str;
-  
-  for(auto i : str){
-      a.push_back(i);
-  }
-  
-  int N;
-  cin >> N;
-  auto cursor = a.end(); //cursor 위치 마지막 글자 오른쪽, iterator
-  
-  while(N--){
-      char cmd;
-      cin >> cmd;
-      if(cmd == 'L'){
-          if(cursor != a.begin()){ //맨 왼쪽 있을때 제외
-              cursor--;
-          }
-      }
-      else if(cmd == 'D'){
-          if(cursor != a.end()){
-              cursor++;
-          }
-      }
-      else if(cmd == 'B'){
-          if(cursor != a.begin()){
-              cursor--;           //cursor를 땡겨서 지울 원소의 위치로 이동
-              cursor = a.erase(cursor); //삭제 후 a.erase()의 return 값은 삭제한 다음 원소를 가리킨다
-              
-          }
-      }
-      else if(cmd == 'P'){
-          char p;
-          cin >> p;
-          a.insert(cursor, p);
-      }
-  }
-  for(auto i : a){
-      cout << i;
-  }
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str;
+    cin >> str;
+
+    list<char> L;
+    for(auto c : str){
+        L.push_back(c);
+    }
+    auto cursor = L.end();
+
+    int M;
+    cin >> M;
+    while(M--){
+        char cmd;
+        cin >> cmd;
+        
+        if(cmd == 'L'){
+            if(cursor != L.begin()){
+                cursor--;
+            }
+        }
+        else if(cmd == 'D'){
+            if(cursor != L.end()){
+                cursor++;
+            }
+        }
+        else if(cmd == 'B'){
+            if(cursor != L.begin()){
+                cursor--;
+                cursor = L.erase(cursor);
+            }
+        }
+        else if(cmd == 'P'){
+            char a;
+            cin >> a;
+            L.insert(cursor, a);
+        }
+    }
+    for(auto c : L){
+        cout << c;
+    }
+
+    return 0;
 }
